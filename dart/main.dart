95fd70815f72acc1debe62ef22709eba0c1870be
baffa <abubakarumar609@gmail.com>
2024-03-03 14:54:25 +0100

    adding files

--- dart/main.dart
+++ dart/main.dart
@@ -1,3 +1,21 @@
 void main(){
-  print("Hello world");
+  
+}
+int addTwo(num1, num2){
+  return num1 + num2;
+}
+int subtractTwo(num1, num2){
+  return num1 - num2;
+}
+int multiplyTwo(num1, num2){
+  return num1 * num2;
+}
+double divideTwo(num1, num2){
+  return num1 / num2;
+}
+int stringLength(String inputString) {
+  return inputString.length;
+}
+E? getFirstElement<E>(List<E> myList) {
+  return myList.isNotEmpty ? myList.first : null;
 }
